fed17f1e525be6631fae38d70890a8c4f6c054cd
rconjoe <root@trog.codes>
2025-02-01 09:59:27 -0800

    change to /tmp

--- src/dlp.ts
+++ src/dlp.ts
@@ -8,14 +8,14 @@ export const server = () => Bun.serve({
       const target = url.searchParams.get("target_url");
       const format = url.searchParams.get("format") || "mp4";
       try {
-        const result = await $`yt-dlp -o './downloads/%(id)s.%(ext)s' -f '${format}' '${target}'`.text();
+        const result = await $`yt-dlp -o '/tmp/ytb/%(id)s.%(ext)s' -f '${format}' '${target}'`.text();
 
         return new Response(result);
       } catch (error) {
         return new Response(`something broke in bun shell's yt-dlp call. logs: \n \n ${error.stdout.toString()} \n \n ${error.stderr.toString()}`)
       }
     }
-    else return new Response("come mierda, 200 OK");
+    else return new Response("404!");
   },
 });
 
