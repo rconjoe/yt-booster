4200688f9f035cd9da21ccc16ff0632e07242176
rconjoe <root@trog.codes>
2025-02-01 08:35:06 -0800

    add some error handling, change OUTPUT TEMPLATE usage to use id instead of video title

--- src/dlp.ts
+++ src/dlp.ts
@@ -7,15 +7,15 @@ export const server = () => Bun.serve({
     if (url.pathname === "/api/download") {
       const target = url.searchParams.get("target_url");
       const format = url.searchParams.get("format") || "mp4";
-      const result = await $`yt-dlp -o '/tmp/%(title)s.%(ext)s' -f '${format}' '${target}'`.text();
+      try {
+        const result = await $`yt-dlp -o './downloads/%(id)s.%(ext)s' -f '${format}' '${target}'`.text();
 
-      return new Response(result);
+        return new Response(result);
+      } catch (error) {
+        return new Response(`something broke in bun shell's yt-dlp call. logs: \n \n ${error.stdout.toString()} \n \n ${error.stderr.toString()}`)
+      }
     }
     else return new Response("come mierda, 200 OK");
   },
 });
-
-
-// man yt-dlp, search for OUTPUT TEMPLATE
-// const result = await $`yt-dlp -o './downloads/%(title)s.%(ext)s' -f mp4 '${target_url}'`.text();
 
