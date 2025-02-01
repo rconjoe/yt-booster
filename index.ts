06b9904106371842a0a8380146698da4b405c3fe
rconjoe <root@trog.codes>
2025-02-01 14:32:20 -0800

    handle /tmp/ytb not existing on startup

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import { server } from "./src/dlp";
 
-server()
+await server()
--- src/dlp.ts
+++ src/dlp.ts
@@ -1,64 +1,84 @@
 import { $ } from "bun";
-import { readdir } from "node:fs/promises";
+import { readdir, access, mkdir } from "node:fs/promises";
 import homepage from "./ui/index.html";
 
-export const server = () => Bun.serve({
-  port: 3400,
-  static: {
-    "/": homepage
-  },
-  async fetch(req) {
-    const url = new URL(req.url);
+async function directoryExists(path: string): Promise<boolean> {
+  try {
+    await access(path);
+    return true; // Directory exists
+  } catch {
+    return false; // Directory does not exist
+  }
+}
 
-    // /api/retrieve
-    if (url.pathname === "/api/retrieve") {
-      const target = url.searchParams.get("target_url");
-      // const format = url.searchParams.get("format") || "mp4";
-      const format = "mp4"
-      const file = (await readdir("/tmp/ytb")).toString();
-      if (file.length > 1) {
-        console.info(`deleting ${file}!`)
-        await Bun.file(`/tmp/ytb/${file}`).delete()
-      } else {
-        console.info("no existing file in /tmp/ytb")
-      }
-      try {
-        console.info(`downloading new file.`)
-        const result = await $`yt-dlp -o '/tmp/ytb/%(id)s.%(ext)s' -f '${format}' '${target}'`.text();
-
-        return new Response(result, {
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
-          }
-        });
-      } catch (error) {
-        console.error(`${error.stdout.toString()}\n\n${error.stderr.toString()}`)
-        return new Response(`ERROR! logs: \n \n ${error.stdout.toString()} \n \n ${error.stderr.toString()}`, { status: 500 })
-      }
+async function dirSetup() {
+  directoryExists("/tmp/ytb").then(async (exists) => {
+    if (exists) {
+      return
     }
+    return await mkdir("/tmp/ytb")
+  });
+}
 
-    // /api/download
-    if (url.pathname === "/api/download") {
-      const file = (await readdir("/tmp/ytb")).toString();
-      if (file.length > 1) {
-        console.info(`serving file: ${file}`)
-        const bunfile = await Bun.file(`/tmp/ytb/${file}`).bytes();
-        return new Response(bunfile, {
-          headers: {
-            "Content-Type": "video/mp4",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Content-Type",
-          }
-        })
-      } else {
-        console.info('/api/download was called, but no file was found in /tmp/ytb')
-        return new Response("no file found", { status: 500 })
+export const server = async () => {
+  await dirSetup()
+  return Bun.serve({
+    port: 3400,
+    static: {
+      "/": homepage
+    },
+    async fetch(req) {
+      const url = new URL(req.url);
+
+      // /api/retrieve
+      if (url.pathname === "/api/retrieve") {
+        const target = url.searchParams.get("target_url");
+        // const format = url.searchParams.get("format") || "mp4";
+        const format = "mp4"
+        const file = (await readdir("/tmp/ytb")).toString();
+        if (file.length > 1) {
+          console.info(`deleting ${file}!`)
+          await Bun.file(`/tmp/ytb/${file}`).delete()
+        } else {
+          console.info("no existing file in /tmp/ytb")
+        }
+        try {
+          console.info(`downloading new file.`)
+          const result = await $`yt-dlp -o '/tmp/ytb/%(id)s.%(ext)s' -f '${format}' '${target}'`.text();
+
+          return new Response(result, {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers": "Content-Type",
+            }
+          });
+        } catch (error) {
+          console.error(`${error.stdout.toString()}\n\n${error.stderr.toString()}`)
+          return new Response(`ERROR! logs: \n \n ${error.stdout.toString()} \n \n ${error.stderr.toString()}`, { status: 500 })
+        }
       }
-    }
 
-    // catch-all 404
-    else return new Response("404!", { status: 404 });
-  },
-});
+      // /api/download
+      if (url.pathname === "/api/download") {
+        const file = (await readdir("/tmp/ytb")).toString();
+        if (file.length > 1) {
+          console.info(`serving file: ${file}`)
+          const bunfile = await Bun.file(`/tmp/ytb/${file}`).bytes();
+          return new Response(bunfile, {
+            headers: {
+              "Content-Type": "video/mp4",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers": "Content-Type",
+            }
+          })
+        } else {
+          console.info('/api/download was called, but no file was found in /tmp/ytb')
+          return new Response("no file found", { status: 500 })
+        }
+      }
 
+      // catch-all 404
+      else return new Response("404!", { status: 404 });
+    },
+  });
+}
